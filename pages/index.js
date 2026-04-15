1672408bd614b4c0ef43571c97dee7dd8a4d6675
iberomundo-viajes <iajes.iberoavmericana.info@gmail.com>
2026-04-16 01:26:53 +0200

    Add Home component with initial content

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>Iberomundo Viajes</h1>
+      <p>La web se está configurando correctamente.</p>
+    </div>
+  )
+}
